0f8bd57add66304bd757734209e50a484af78449
Frantisek kolar <fkolar@kolar.pro>
2016-04-02 00:59:39 +0200

    fixed jsLInt errors

--- src/app/authoring/create-form/templates/templates.js
+++ src/app/authoring/create-form/templates/templates.js
@@ -5,7 +5,7 @@ var authoring = angular.module('sFormApp.authoring');
 
 authoring.controller('CreateFormTemplatesCtrl', function ($scope) {
     var vm = this;
-    vm.templates = ["Template 1", "Template 2"];
+    vm.templates = ['Template 1', 'Template 2'];
 
 
     vm.onTabSelected = function (title) {
